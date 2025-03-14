479f764948e138fe16832dfa2ef5da9aef4d0473
found-cake <me@foundcake.kr>
2025-03-14 04:31:59 +0000

    testcode

--- ginsecurity_test.go
+++ ginsecurity_test.go
@@ -3,6 +3,7 @@ package ginsecurity_test
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 
 	"github.com/found-cake/ginsecurity"
@@ -15,7 +16,8 @@ import (
 
 const resp_msg = "bar"
 
-func setupRouter(config *config.SecurityConfig) *gin.Engine {
+func setupFooRouter(config *config.SecurityConfig) *gin.Engine {
+	gin.SetMode(gin.TestMode)
 	r := gin.Default()
 	r.Use(ginsecurity.New(config))
 	r.GET("/foo", func(c *gin.Context) {
@@ -24,18 +26,24 @@ func setupRouter(config *config.SecurityConfig) *gin.Engine {
 	return r
 }
 
-func TestNOConf(t *testing.T) {
-	r := setupRouter(&config.SecurityConfig{})
+func requestFoo(conf *config.SecurityConfig) *httptest.ResponseRecorder {
+	r := setupFooRouter(conf)
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/foo", nil)
+	req, _ := http.NewRequest(http.MethodGet, "/foo", nil)
 	r.ServeHTTP(w, req)
+
+	return w
+}
+
+func TestNOConf(t *testing.T) {
+	w := requestFoo(&config.SecurityConfig{})
 
 	assert.Equal(t, http.StatusOK, w.Code)
 	assert.Equal(t, resp_msg, w.Body.String())
 }
 
 func TestWriteHeader(t *testing.T) {
-	r := setupRouter(&config.SecurityConfig{
+	w := requestFoo(&config.SecurityConfig{
 		STS:                config.DefaultSTSConfig(),
 		IENoOpen:           true,
 		NoStoreCache:       true,
@@ -43,10 +51,42 @@ func TestWriteHeader(t *testing.T) {
 		FrameDeny:          true,
 		BrowserXssFilter:   utils.XSS_BLOCK,
 	})
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/foo", nil)
-	r.ServeHTTP(w, req)
-
 	header := w.Header()
+
+	assert.NotEmpty(t, header.Get(h.XDownloadOpotions))
+	assert.NotEmpty(t, header.Get(h.CacheControl))
+	assert.NotEmpty(t, header.Get(h.Pragma))
+	assert.NotEmpty(t, header.Get(h.Expires))
+	assert.NotEmpty(t, header.Get(h.XContentTypeOptions))
+	assert.NotEmpty(t, header.Get(h.XFrameOptions))
 	assert.NotEmpty(t, header.Get(h.StrictTransportSecurity))
+}
+
+func TestSSLRedirect(t *testing.T) {
+	w := requestFoo(&config.SecurityConfig{
+		SSLConfig: &config.SSLConfig{
+			IsRedirect: true,
+		},
+	})
+
+	assert.Equal(t, http.StatusMovedPermanently, w.Code)
+}
+
+func TestSSLRedirectOtherHost(t *testing.T) {
+	const testHost = "gin-gonic.com"
+	w := requestFoo(&config.SecurityConfig{
+		SSLConfig: &config.SSLConfig{
+			IsRedirect: true,
+			Host:       testHost,
+		},
+	})
+	unescpaeurl, _ := url.PathUnescape(w.Header().Get("Location"))
+	url, _ := url.Parse(unescpaeurl)
+
+	assert.Equal(t, http.StatusMovedPermanently, w.Code)
+	assert.Equal(t, testHost, url.Host)
+}
+
+func TestCorsAllowAll(t *testing.T) {
+	//TODO
 }
